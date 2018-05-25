d778bba72f109f478834a5b3172f179663efa3a6
Anshul2166 <anshuldubey2166@gmail.com>
2018-05-25 18:58:04 +0530

    Switching tabs working

--- src/Components/Homepage/OptionsRow/OptionsRow.js
+++ src/Components/Homepage/OptionsRow/OptionsRow.js
@@ -6,16 +6,32 @@ import Tab from "@material-ui/core/Tab";
 import Button from "@material-ui/core/Button";
 import "./OptionsRow.css";
 
+const lists = [
+	"All",
+	"Web Development",
+	"Android Development",
+	"Graphic Design",
+	"Machine Learning"
+];
+
 class OptionsRow extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { value: 0 };
+		this.onClickTab = this.onClickTab.bind(this);
+	}
+	onClickTab(event,value) {
+		this.setState({ value });
+	}
+
 	render() {
+		const listCategories = lists.map(name => (
+			<Tab label={name} value={name}/>
+		));
 		return (
 			<div className="OptionsRow">
 				<AppBar position="static" color="default">
-					<Tabs value={0}>
-						<Tab label={<Badge>Item One</Badge>} />
-						<Tab label="Item Two" />
-						<Tab label="Item Three" />
-					</Tabs>
+					<Tabs value={this.state.value} onChange={this.onClickTab}>{listCategories}</Tabs>
 				</AppBar>
 			</div>
 		);
