04a050769778791e958c6902a8ae96abf51c8e1a
saivicky2015 <saivicky2015@gmail.com>
2018-06-15 15:12:08 +0530

    added the overview

--- src/models/staticModel.js
+++ src/models/staticModel.js
@@ -1,90 +1,127 @@
 const keywordsList = ["XML", "HTTP", "NodeJs"];
 
 const BrowserSupportList = [
-	{
-		browserName: "Chrome",
-		support: "true"
-	},
-	{
-		browserName: "Firefox",
-		support: "true"
-	},
-	{
-		browserName: "InternetExplorer",
-		support: "true"
-	},
-	{
-		browserName: "Opera",
-		support: "true"
-	},
-	{
-		browserName: "Edge",
-		support: "true"
-	}
+  {
+    browserName: "Chrome",
+    support: "true"
+  },
+  {
+    browserName: "Firefox",
+    support: "true"
+  },
+  {
+    browserName: "InternetExplorer",
+    support: "true"
+  },
+  {
+    browserName: "Opera",
+    support: "true"
+  },
+  {
+    browserName: "Edge",
+    support: "true"
+  }
 ];
 
 const details = [
-	{
-		name: "npm",
-		content: "v18.0.0"
-	},
-	{
-		name: "build",
-		content: "failing"
-	},
-	{
-		name: "chat",
-		content: "gitter"
-	},
-	{
-		name: "contributors",
-		content: "4"
-	}
+  {
+    name: "npm",
+    content: "v18.0.0"
+  },
+  {
+    name: "build",
+    content: "failing"
+  },
+  {
+    name: "chat",
+    content: "gitter"
+  },
+  {
+    name: "contributors",
+    content: "4"
+  }
 ];
 
 const features = [
-	{
-		descripton: "Find Partners for project"
-	},
-	{
-		descripton: "Able to open source the project"
-	},
-	{
-		descripton: "Supports pair programming"
-	},
-	{
-		descripton: "Tracks specific user according to their skills"
-	},
-	{
-		descripton: "Able to launch call for help in sorting issue"
-	}
+  {
+    descripton: "Find Partners for project"
+  },
+  {
+    descripton: "Able to open source the project"
+  },
+  {
+    descripton: "Supports pair programming"
+  },
+  {
+    descripton: "Tracks specific user according to their skills"
+  },
+  {
+    descripton: "Able to launch call for help in sorting issue"
+  }
 ];
 
 const resources = [
-	{
-		title: "How to Contribute",
-		link: "#"
-	},
-	{
-		title: "Stacks Used",
-		link: "#"
-	}
+  {
+    title: "How to Contribute",
+    link: "#"
+  },
+  {
+    title: "Stacks Used",
+    link: "#"
+  }
 ];
-
 
 const installation = [
-	{
-		title: "Using npm",
-		code: "npm start"
-	},
-	{
-		title: "Using yarn",
-		code: "yarn run"
-	},
-	{
-		title: "On deployed version",
-		code: "https://www.projectongithub.io"
-	}
+  {
+    title: "Using npm",
+    code: "npm start"
+  },
+  {
+    title: "Using yarn",
+    code: "yarn run"
+  },
+  {
+    title: "On deployed version",
+    code: "https://www.projectongithub.io"
+  }
 ];
 
-export {keywordsList,BrowserSupportList,details,features,resources,installation};
+const title="Sample Project";
+
+const overviews = [
+  {
+    name: "Version",
+    content: "v18.0.0"
+  },
+  {
+    name: "License",
+    content: "MIT"
+  },
+  {
+    name: "Open Issues",
+    content: "4"
+  },
+  {
+    name: "Repository",
+    content: "Openbud"
+  },
+  {
+    name: "Collabrators",
+    content: "Vikranth, Anshul"
+  }
+];
+
+
+const description="When an open source project starts, it is normally accompanied by a single person and only when that person does a lot of work and gets the project up and running, do other open source contributors come to"
+
+export {
+  keywordsList,
+  BrowserSupportList,
+  details,
+  features,
+  resources,
+  installation,
+  overviews,
+  title,
+  description
+};
