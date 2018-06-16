86d98bc5b02cb9c620dc0e2b395e356c09c9a651
saivicky2015 <saivicky2015@gmail.com>
2018-06-16 22:59:42 +0530

    populated the projects and users while returning in rootquery

--- server_graphql/schema/RootQueryType.js
+++ server_graphql/schema/RootQueryType.js
@@ -13,27 +13,27 @@ const User=require("../models/User");
         projects:{
             type:new GraphQLList(ProjectType),
             resolve(parentValues,args,request){
-                return Project.find();
+                return Project.find().populate("contributors");
             }
         },
         project:{
             type:ProjectType,
             args:{id:{type:new GraphQLNonNull(GraphQLID)}},
             resolve(parentValue,args,request){
-                return Project.findById(args.id)
+                return Project.findById(args.id).populate("contributors")
             }
         },
         users:{
             type:new GraphQLList(UserType),
             resolve(parentValues,args,request){
-                return User.find();
+                return User.find().populate("projects");
             }
         },
         user:{
             type:UserType,
             args:{id:{type:new GraphQLNonNull(GraphQLID)}},
             resolve(parentValue,args,request){
-                return User.findById(args.id)
+                return User.findById(args.id).populate("projects")
             }
         },
         
